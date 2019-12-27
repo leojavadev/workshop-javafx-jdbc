1b8264cf71ab591b5deb48fd156d364c9a6354c3
leojavadev <leojavadev@gmail.com>
2019-12-27 20:03:39 -0300

    MainViewController - primeira implementação

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,5 +1,41 @@
 package gui;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+	
+	@FXML
+	private MenuItem menuItemSeller;
+	
+	@FXML
+	private MenuItem menuItemDepartment;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	public void onMenuItemSeller() {
+		Alerts.showAlert(null, null, "Seller", AlertType.INFORMATION);
+	}
+	
+	public void onMenuItemDepartment() {
+		Alerts.showAlert(null, null, "Department", AlertType.INFORMATION);		
+	}
+	
+	public void onMenuItemAbout() {
+		Alerts.showAlert(null, null, "About", AlertType.INFORMATION);
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		
+	}
 
 }
